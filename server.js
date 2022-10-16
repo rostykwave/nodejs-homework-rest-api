31939d28912ba6e603ffab88d0cb3e3bb62fb655
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-10-16 22:00:18 +0300

    @ PATCH , updateStatusContact

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() =>
+  .then(() => {
+    console.log('Database connection successful');
+
     app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: PORT`);
-    })
-  )
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
--- src/services/contactsService.js
+++ src/services/contactsService.js
@@ -1,80 +1,49 @@
-const fs = require('node:fs/promises');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-
-const contactsPath = path.resolve('src/db/contacts.json');
+const { Contact } = require('../models/contact');
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
+  const contacts = await Contact.find({}, '-createdAt -updatedAt');
 
   return contacts;
 };
 
 const getById = async contactId => {
-  const contacts = await listContacts();
-  const contactById = contacts.find(contact => contact.id === contactId);
+  const contactById = await Contact.findById(
+    contactId,
+    '-createdAt -updatedAt'
+  );
 
   return contactById;
 };
 
 const addContact = async body => {
-  const { name, email, phone } = body;
-
-  const contacts = await listContacts();
-
-  const newContact = {
-    id: uuidv4(),
-    name,
-    email,
-    phone,
-  };
-
-  const updatedContacts = [...contacts, newContact];
-
-  fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+  const newContact = await Contact.create(body);
 
   return newContact;
 };
 
 const removeContact = async contactId => {
-  const contacts = await listContacts();
-
-  const indexOfRemovingContact = contacts.findIndex(
-    contact => contact.id === contactId
-  );
-
-  if (indexOfRemovingContact === -1) {
-    return null;
-  }
-
-  const removedContactByID = contacts[indexOfRemovingContact];
-  contacts.splice(indexOfRemovingContact, 1);
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  const removedContactByID = await Contact.findByIdAndRemove(contactId);
 
   return removedContactByID;
 };
 
 const updateContact = async (contactId, body) => {
-  const contacts = await listContacts();
-
-  const indexOfUpdatingContact = contacts.findIndex(
-    contact => contact.id === contactId
-  );
-
-  if (indexOfUpdatingContact === -1) {
-    return undefined;
-  }
-  const updatingContact = contacts[indexOfUpdatingContact];
-
-  const { name, email, phone } = body;
-  updatingContact.name = name;
-  updatingContact.email = email;
-  updatingContact.phone = phone;
-
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  const updatingContact = await Contact.findByIdAndUpdate(contactId, body, {
+    new: true,
+  });
 
   return updatingContact;
+};
+const updateStatusContact = async (contactId, body) => {
+  const updatingStatusContact = await Contact.findByIdAndUpdate(
+    contactId,
+    body,
+    {
+      new: true,
+    }
+  );
+
+  return updatingStatusContact;
 };
 
 module.exports = {
@@ -83,4 +52,5 @@ module.exports = {
   addContact,
   removeContact,
   updateContact,
+  updateStatusContact,
 };
