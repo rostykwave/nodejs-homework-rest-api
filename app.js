e3848068f55c9514e77dc8286f7ec0258beeca4e
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-10-16 19:36:33 +0300

    .env linked

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const { DB_HOST } = require('./config');
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
